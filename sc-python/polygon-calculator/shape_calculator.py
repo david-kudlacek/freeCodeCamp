950f7abbb8dff78b68f9afa17f5777e6c2b01ba7
David Kudláček <david.kudlacek.it@gmail.com>
2023-09-26 18:59:09 +0200

    Initialization vars for class

--- sc-python/polygon-calculator/shape_calculator.py
+++ sc-python/polygon-calculator/shape_calculator.py
@@ -1,6 +1,9 @@
 class Rectangle:
-
-
+    def __init__(self, width, height):
+        self.width = width
+        self.height = height
 
 
 class Square:
+    def __init__(self, length):
+        self.length = length
